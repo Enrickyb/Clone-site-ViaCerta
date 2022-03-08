513e1f5a9c270174cb3ec81a2d59be5c85684bcc
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-03-08 18:22:29 -0400

    att barras

--- menu.js
+++ menu.js
@@ -1,7 +1,11 @@
 var ativar = document.getElementById('mde')
+var lg = document.getElementById('logo')
+var barra = document.querySelector('.one')
+var barra2 = document.querySelector('.three')
 var show = true
+
 function turnon() {
-    ativar.classList.add('menu-section-on', show)
+    ativar.classList.toggle('menu-section-on', show)
     show = !show
 }
 
