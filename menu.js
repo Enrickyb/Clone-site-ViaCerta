c26acfc4ed76d2c60762558ac2b5004e3766c630
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-03-09 08:05:10 -0400

    Começando o carrossel

--- menu.js
+++ menu.js
@@ -3,11 +3,26 @@ var lg = document.getElementById('logo')
 var barra = document.querySelector('.one')
 var barra2 = document.querySelector('.three')
 var show = true
-
+//menu hamburger
 function turnon() {
     ativar.classList.toggle('menu-section-on', show)
     show = !show
 }
+
+//carrossel
+const imgs = document.getElementById('img')
+const img = document.querySelectorAll('#img img')
+
+let idx = 0
+
+function carrossel() {
+    idx++
+    if(idx > img.length - 1) { //retorna pra primeira imagem
+        idx = 0
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+setInterval(carrossel, 3000)
 
 
 
