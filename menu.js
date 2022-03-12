72507417c0419875944bcb43f5058d9e12c35b80
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-03-12 17:00:59 -0400

    add scroll animation

--- menu.js
+++ menu.js
@@ -1,7 +1,5 @@
 var ativar = document.getElementById('mde')
 var lg = document.getElementById('logo')
-var barra = document.querySelector('.one')
-var barra2 = document.querySelector('.three')
 var show = true
 //menu hamburger
 function turnon() {
@@ -9,7 +7,8 @@ function turnon() {
     show = !show
 }
 
-//carrossel
+
+//slide 
 const imgs = document.getElementById('img')
 const img = document.querySelectorAll('#img img')
 
@@ -17,12 +16,67 @@ let idx = 0
 
 function carrossel() {
     idx++
-    if(idx > img.length - 1) { //retorna pra primeira imagem
+    if(idx > img.length - 4) { //retorna pra primeira imagem
         idx = 0
     }
-    imgs.style.transform = `translateX(${-idx * 500}px)`
+    imgs.style.transform = `translateX(${-idx * 200}px)`
 }
 setInterval(carrossel, 3000)
 
 
 
+
+
+
+//animação scroll
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function() {
+            timeout = null;
+            if(!immediate) func.apply(context, args)
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow) func.apply(context, args);
+    };
+};
+
+///
+
+
+const target = document.querySelectorAll('[data-anime]')
+
+const animationClass = 'animate'
+
+
+function animeScroll() {
+
+    const windowTop = window.scrollY + ((window.innerHeight * 3)/ 3) 
+
+    
+    target.forEach(function(element){
+       
+      
+        if(windowTop > element.offsetTop){ 
+            
+            element.classList.add(animationClass)
+        } else {
+            
+            element.classList.remove(animationClass)
+        }
+    console.log(element.offsetTop)
+    })
+}
+
+animeScroll() 
+if(target.length) { 
+
+window.addEventListener('scroll', debounce(function(){
+    animeScroll()
+    console.log("")
+},100))
+}
